93233d4f4ab9b33081e9344355a876896b28613b
Alex Olivier <alex@alexolivier.me>
2024-07-09 17:15:51 +0100

    add cerbos
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const path = require('path')
+const Cerbos = require("@cerbos/grpc");
+const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 5001
+const cerbosClient = new Cerbos.GRPC("localhost:3593", { tls: false });
+
+const PORT = process.env.PORT || 5001;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/cerbos", async (req, res) => {
+    const result = await cerbosClient.serverInfo();
+    res.json(result);
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
