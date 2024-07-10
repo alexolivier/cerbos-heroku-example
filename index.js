4c97e7c5e7383dfeec145bff762198d83ad6800f
Alex Olivier <alex@alexolivier.me>
2024-07-10 09:06:52 +0100

    make check call
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- index.js
+++ index.js
@@ -10,9 +10,32 @@ express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
-  .get("/cerbos", async (req, res) => {
-    const result = await cerbosClient.serverInfo();
+  .get("/", async (req, res) => {
+    const result = await cerbosClient.checkResource({
+      requestId: "5652d164-54ae-4396-bcf8-403367402442",
+      resource: {
+        kind: "expense",
+        id: "expense1",
+        attr: {
+          amount: 500,
+          approvedBy: null,
+          createdAt: "2024-04-17T10:03:02.011Z",
+          ownerId: "sally",
+          region: "EMEA",
+          status: "OPEN",
+          vendor: "Flux Water Gear",
+        },
+      },
+      principal: {
+        id: "derek",
+        roles: ["USER", "MANAGER"],
+        attr: {
+          department: "FINANCE",
+          region: "EMEA",
+        },
+      },
+      actions: ["view", "view:approver"],
+    });
     res.json(result);
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
